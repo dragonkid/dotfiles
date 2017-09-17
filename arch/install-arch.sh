e5143772607f1076025e4752deb896c2efe87a5e
dragonkid <idragonkid@gmail.com>
2017-09-17 20:48:57 +0800

    add configuration file of terminator & add autojump to install script

--- arch/install-arch.sh
+++ arch/install-arch.sh
@@ -2,7 +2,7 @@
 SCRIPT="$(cd "$(dirname "$0")" && pwd -P)"/"$(basename "$0")"
 BASEDIR=$(dirname "${SCRIPT}")
 
-sudo pacman -Syu && sudo pacman -Sy base-devel zsh git tmux htop wget bmon keychain lsof terminator
+sudo pacman -Syu && sudo pacman -Sy base-devel zsh git tmux htop wget bmon keychain lsof terminator autojump
 
 # config vim
 VIM_RUNTIME=~/.vim_runtime
